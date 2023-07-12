69604b95b8d39c9ef10b45ef13bb61fb01ef6b63
dnjsxoghd <feelchok1234@gmail.com>
2023-07-12 23:46:24 +0900

    ArrayList

---
+++ JavaDemo/src/Array/StudentArrayList.java
@@ -0,0 +1,55 @@
+package Array;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class StudentArrayList {
+
+    private String HWT;
+    private ArrayList<Integer> marks = new ArrayList<Integer>();
+
+    public StudentArrayList(String string, int... marks) {
+        this.HWT=HWT;
+        for(int mark : marks){
+            this.marks.add(mark);
+        };
+    }
+
+    public int getNumber(){
+        return marks.size();
+    }
+
+    public int getTotal() {
+        int sum=0;
+        for(int m1:marks){
+            sum+=m1;
+        }
+
+        return sum;
+    }
+
+    // for문은 정석을 좀 알아보자
+    public int getMax() {
+
+        return Collections.max(marks);
+    }
+
+    public int getMin() {
+
+        return Collections.min(marks);
+    }
+
+    public BigDecimal getAverage(){
+        int sum=getTotal();
+        int number = getNumber();
+
+        return new BigDecimal(sum).divide(new BigDecimal(number), 3,RoundingMode.UP);
+    }
+
+    public String toString(){
+        return HWT+marks;
+    }
+
+}
---
+++ JavaDemo/src/Array/StudentRunnerArrayList.java
@@ -0,0 +1,38 @@
+package Array;
+
+import java.math.BigDecimal;
+
+public class StudentRunnerArrayList {
+
+    // 매개변수로 배열 객체를 줄 때는 new 타입 [] {n,n,n...}
+
+    StudentArrayList student = new StudentArrayList("HWT", 99,98,1000);
+    StudentArrayList student1 = new StudentArrayList("홍원태", 44,55,66);
+    StudentArrayList student2 = new StudentArrayList("HWT", 77,88,99,100);
+
+
+    int number1 = student1.getNumber();
+
+    int number2 = student1.getTotal();
+
+    int number3 = student1.getMax();
+
+    int number4 = student1.getMin();
+
+    BigDecimal number5 = student1.getAverage();
+
+    public static void main(String[] args) {
+
+        StudentRunnerArrayList s1 = new StudentRunnerArrayList();
+
+        System.out.println(s1.number1);
+        System.out.println(s1.number2);
+        System.out.println(s1.number3);
+        System.out.println(s1.number4);
+        System.out.println(s1.number5);
+
+        System.out.println(s1.student);
+
+
+    }
+}
